1b204356aed33d4fcf7bbb87ace912d5f5ea05d2
TToushirou <46146484+TToushirou@users.noreply.github.com>
2022-01-26 17:31:40 +0000

    Update

--- app/js/script.js
+++ app/js/script.js
@@ -4,35 +4,3 @@ const test = () => {
 	console.log('this is a test');
 };
 
-
-// Hamburger 
-let path = document.querySelector(".path");
-
-console.log(path.getAttribute('d'))
-
-function lerp(start, end, t){
-    return start * (1 - t) + end * t;
-}
-
-let toggle = false;
-
-let navToggle = document.querySelector('.nav-tog');
-let navWrapper = document.querySelector('.wrapper');
-menuToggle.addEventListener('click', () => {
-    setTimeout(() => {
-        toggle = !toggle;
-       
-    }, 300);
-
-	if(toggle){
-		navWrapper.classList.remove('active');
-	} else {
-		setTimeout(() => {
-			navWrapper.classList.add('active');
-		   
-		}, 300);
-	}
-
-	menuToggle.classList.toggle('active');
-
-});
