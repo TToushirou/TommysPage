6154ffd5d42f39a7c42b4e2adfdf36e8fd9b3d3c
TToushirou <46146484+TToushirou@users.noreply.github.com>
2022-03-02 11:37:13 +0000

    rebanded
    portfolio re brand

--- app/js/script.js
+++ app/js/script.js
@@ -4,3 +4,34 @@ const test = () => {
 	console.log('this is a test');
 };
 
+let path = document.querySelector(".path");
+
+console.log(path.getAttribute('d'))
+
+function lerp(start, end, t){
+    return start * (1 - t) + end * t;
+}
+
+let toggle = false;
+
+let menuToggle = document.querySelector('.nav__tog');
+let navWrapper = document.querySelector('.nav__wrapper');
+menuToggle.addEventListener('click', () => {
+    setTimeout(() => {
+        toggle = !toggle;
+       
+    }, 300);
+
+	if(toggle){
+		navWrapper.classList.remove('active');
+	} else {
+		setTimeout(() => {
+			navWrapper.classList.add('active');
+		   
+		}, 300);
+	}
+
+	menuToggle.classList.toggle('active');
+
+});
+
